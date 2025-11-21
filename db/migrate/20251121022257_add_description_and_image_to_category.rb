542043fd9b9e902b9032eb75fb88e7c87b07f43f
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2025-11-20 18:29:10 -0800

    added categories table

---
+++ db/migrate/20251121022257_add_description_and_image_to_category.rb
@@ -0,0 +1,5 @@
+class AddDescriptionAndImageToCategory < ActiveRecord::Migration[7.1]
+  def change
+    add_column :categories, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_16_213304) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_21_022257) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -59,6 +59,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_16_213304) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
   end
 
   create_table "products", force: :cascade do |t|
